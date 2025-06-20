a066fc906beffca3b3f8869c5ede04e4cc86134f
AndreRicardoé <contatoadmx@gmail.com>
2025-06-20 12:04:59 -0300

    primary key in __config sqlite

--- source/PhpMx/Datalayer/Connection/Sqlite.php
+++ source/PhpMx/Datalayer/Connection/Sqlite.php
@@ -74,7 +74,7 @@ class Sqlite extends BaseConnection
                 ->limit(1);
 
             if (!count($this->executeQuery($configTableExistsQuery)))
-                $this->executeQuery('CREATE TABLE __config (`name` VARCHAR (100), `value` TEXT);');
+                $this->executeQuery('CREATE TABLE __config (name TEXT PRIMARY KEY, value TEXT);');
 
             foreach ($this->executeQuery(Query::select('__config')) as $config)
                 $this->config[$config['name']] = is_serialized($config['value']) ? unserialize($config['value']) : $config['value'];
